69df7a206a49e92585d63469fd8829aeb9641da1
Jack-Stagger <112420969+Jack-Stagger@users.noreply.github.com>
2022-09-01 18:22:50 +0900

    프로젝트 에셋 추가
    Project Asset 내의 폴더 및 파일들을 추가하였습니다.

---
+++ Assets/Project Assets/3D Models/Old Tractor/Scripts/CameraSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera[] cameras;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cameras == null)
+            return;
+
+        cameras[0].gameObject.SetActive(true);
+
+        for (int i = 1; i < cameras.Length; i++)
+        {
+            cameras[i].gameObject.SetActive(false);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        float offsetY = 10;
+
+        if (cameras == null)
+            return;
+
+        for(int i=0;i<cameras.Length;i++)
+        {
+            offsetY = 10 + 40 * i;
+
+            if (GUI.Button(new Rect(10,offsetY,100,35),cameras[i].name))
+            {
+                for(int j=0;j<cameras.Length;j++)
+                {
+                    if (j==i)
+                    {
+                        cameras[i].gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        cameras[j].gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
